2c50f839bc75789fe24c11b7a6a599840ed42c57
Ben Boeckel <ben.boeckel@kitware.com>
2014-10-03 17:52:30 -0400

    vtkWin32OpenGLRenderWindow: use size_t for integer storage of pointers
    Also explicitly reinterpret_cast them.
    
    Change-Id: I5ec4ae799e205e743c868cecffcf9ee400ded60f

--- Rendering/OpenGL/vtkWin32OpenGLRenderWindow.cxx
+++ Rendering/OpenGL/vtkWin32OpenGLRenderWindow.cxx
@@ -1348,19 +1348,19 @@ void vtkWin32OpenGLRenderWindow::SetWindowId(HWND arg)
 // Set this RenderWindow's X window id to a pre-existing window.
 void vtkWin32OpenGLRenderWindow::SetWindowInfo(char *info)
 {
-  int tmp;
-  sscanf(info, "%i", &tmp);
+  size_t tmp;
+  sscanf(info, "%zu", &tmp);
 
-  this->WindowId = (HWND)tmp;
+  this->WindowId = reinterpret_cast<HWND>(tmp);
   vtkDebugMacro(<< "Setting WindowId to " << this->WindowId << "\n");
 }
 
 void vtkWin32OpenGLRenderWindow::SetNextWindowInfo(char *info)
 {
-  int tmp;
-  sscanf(info, "%i", &tmp);
+  size_t tmp;
+  sscanf(info, "%zu", &tmp);
 
-  this->SetNextWindowId((HWND)tmp);
+  this->SetNextWindowId(reinterpret_cast<HWND>(tmp));
 }
 
 void vtkWin32OpenGLRenderWindow::SetDisplayId(void * arg)
@@ -1382,10 +1382,10 @@ void vtkWin32OpenGLRenderWindow::SetDeviceContext(HDC arg)
 // Sets the HWND id of the window that WILL BE created.
 void vtkWin32OpenGLRenderWindow::SetParentInfo(char *info)
 {
-  int tmp;
-  sscanf(info, "%i", &tmp);
+  size_t tmp;
+  sscanf(info, "%zu", &tmp);
 
-  this->ParentId = (HWND)tmp;
+  this->ParentId = reinterpret_cast<HWND>(tmp);
   vtkDebugMacro(<< "Setting ParentId to " << this->ParentId << "\n");
 }
 
